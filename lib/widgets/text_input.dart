d236c17da3ef8a7bc50a15f45c264c5764d1cf84
matheusrmatias <matias.matheus@aluno.ifsp.edu.br>
2025-05-17 12:00:27 -0300

    Resolve format document

--- lib/widgets/text_input.dart
+++ lib/widgets/text_input.dart
@@ -21,26 +21,34 @@ class TextInput extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: margin ?? EdgeInsets.symmetric(horizontal: 16,vertical: 12),
+      margin: margin ?? EdgeInsets.symmetric(horizontal: 16, vertical: 12),
       child: TextFormField(
         style: TextStyle(fontSize: 18),
         controller: controller,
         maxLines: maxlines,
         decoration: InputDecoration(
-            border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(12))),
-            fillColor: backgroundColor,
-            filled: true,
-            label: Container(
-                padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-                decoration: BoxDecoration(
-                    color: backgroundColor,
-                    borderRadius: BorderRadius.all(Radius.circular(8))),
-                child: Text(
-                  label,
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                )),
-            suffixIcon: Icon(icon)),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(12),
+            ),
+          ),
+          fillColor: backgroundColor,
+          filled: true,
+          label: Container(
+            padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+            decoration: BoxDecoration(
+              color: backgroundColor,
+              borderRadius: BorderRadius.all(
+                Radius.circular(8),
+              ),
+            ),
+            child: Text(
+              label,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          suffixIcon: Icon(icon),
+        ),
       ),
     );
   }
